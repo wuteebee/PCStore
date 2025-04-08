1a610a410c6510fc0fcd91b00d13758e4740e2f1
wuteebee <185331818+wuteebee@users.noreply.github.com>
2025-04-08 16:11:09 +0700

    cleaning code

--- src/DTO/ThongSoKyThuat.java
+++ src/DTO/ThongSoKyThuat.java
@@ -5,18 +5,15 @@ public class ThongSoKyThuat {
     private String idDanhMuc;
     private String tenThongSo;
 
-    // Default constructor
     public ThongSoKyThuat() {
     }
 
-    // Constructor with parameters
     public ThongSoKyThuat(String idThongSo, String idDanhMuc, String tenThongSo) {
         this.idThongSo = idThongSo;
         this.idDanhMuc = idDanhMuc;
         this.tenThongSo = tenThongSo;
     }
 
-    // Getters and Setters
     public void setIdDanhMuc(String idDanhMuc) {
         this.idDanhMuc = idDanhMuc;
     }
